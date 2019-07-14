1ef45cc5527373e2137013834656c336a067fe48
LennartJagt <lvdjagt@gmail.com>
2019-07-14 18:58:38 +0200

    verder met inzicht_app

---
+++ db/migrate/20190714144753_create_practices.rb
@@ -0,0 +1,10 @@
+class CreatePractices < ActiveRecord::Migration[5.2]
+  def change
+    create_table :practices do |t|
+      t.string :name
+      t.string :city
+
+      t.timestamps
+    end
+  end
+end
